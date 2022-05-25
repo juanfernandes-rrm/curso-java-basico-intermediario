f68749ebecc8a71e496d955be8fa16e45ac67274
Juan Fernandes <juanfernandesrrm@gmail.com>
2022-05-25 11:18:36 -0300

    aula 75

---
+++ src/main/java/com/juan/cursojava/aula75_84/Aula75.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juan.cursojava.aula75_84;
+
+/**
+ *
+ * @author juann
+ */
+public class Aula75 {
+    public static void main(String[] args) {
+        //criando uma string
+        String vazia = new String();
+        
+        String java = new String("JAVA");
+        String java1 = new String(java);
+        
+        char[] charsJava = {'J','A','V','A'};
+        String java2 = new String(charsJava);
+        
+        
+    }
+}
