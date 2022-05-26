f015b9f86d8dcaa312b4862ffc8dcee412637a7e
Juan Fernandes <juanfernandesrrm@gmail.com>
2022-05-25 23:38:58 -0300

    aula 81

---
+++ src/main/java/com/juan/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juan.cursojava.aula75_84;
+
+/**
+ *
+ * @author juann
+ */
+public class Aula81 {
+    public static void main(String[] args) {
+        String teste = "Teste";
+        
+        //toLowerCase - transforma todos os carac. em minisculo
+        String testeMinsc = teste.toLowerCase();
+        //toUpperCase - transforma todos os carac. em maiusculo
+        String testeMaiusc = teste.toUpperCase();
+        System.out.println(testeMinsc);
+        System.out.println(testeMaiusc);
+    }
+}
