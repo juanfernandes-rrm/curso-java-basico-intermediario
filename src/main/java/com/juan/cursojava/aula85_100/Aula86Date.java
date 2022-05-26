0135efee41b0167c8b7185972bb006c3a0610f2c
Juan Fernandes <juanfernandesrrm@gmail.com>
2022-05-26 15:29:21 -0300

    aula 86

---
+++ src/main/java/com/juan/cursojava/aula85_100/Aula86Date.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juan.cursojava.aula85_100;
+
+import java.util.Date;
+
+/**
+ *
+ * @author juann
+ */
+public class Aula86Date {
+    public static void main(String[] args) {
+        Date hoje = new Date();
+        System.out.println(hoje);
+        
+        System.out.println("Quantidade de milisegundos desde 1 jan 1970"+ hoje.getTime());
+    }
+}
