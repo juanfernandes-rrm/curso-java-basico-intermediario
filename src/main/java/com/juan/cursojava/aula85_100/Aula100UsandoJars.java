6079612e5751a168442f9e263e017354ed65ee2a
Juan Fernandes <juanfernandesrrm@gmail.com>
2022-06-08 15:38:14 -0300

    aula 100

---
+++ src/main/java/com/juan/cursojava/aula85_100/Aula100UsandoJars.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juan.cursojava.aula85_100;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author juann
+ */
+public class Aula100UsandoJars {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("curso java"));
+    }
+}
